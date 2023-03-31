36734f0b1ac6aa24728bb5bb23669d42ddaa5fa9
Patrick Spauster <patrick.spauster@gmail.com>
2023-03-30 20:25:03 -0400

    added data constant
    still need to get the this call to reference the right item in the list and pull the name

--- js/script.js
+++ js/script.js
@@ -10,7 +10,8 @@ function writeHtml(name, img) {
 $('.bedstuy').on('click', function () {
     $('.cd-tile').removeClass("selected")
     writeHtml("Chi Osse", "https://www.politico.com/dims4/default/d2db57a/2147483647/strip/true/crop/2000x2558+0+0/resize/630x806!/quality/90/?url=https%3A%2F%2Fstatic.politico.com%2Fc5%2Fee%2Fbb4cc3794eada341211d95cdf70f%2Fmag.McHugh.Osse.portrait.jpg");
-    $('.bedstuy').addClass("selected")
+    $(this).addClass("selected")
+    console.log(this)
 })
 
 $('.les').on('click', function () {
@@ -25,3 +26,24 @@ $('.lic').on('click', function () {
     $('.lic').addClass("selected")
 })
 
+$('.cd-tile').on('click', function () {
+    $('.cd-tile').removeClass("selected")
+    console.log("class =", this.classname);
+    writeHtml(data.lic.name, data.lic.imageURL);
+    $(this).addClass("selected")
+})
+
+const data = {
+    lic: {
+        name: "Julie Won",
+        imageURL: "https://pbs.twimg.com/profile_images/1489071521079468033/UiG1DC-z_400x400.jpg"
+    },
+    les: {
+        name: "Carlina Rivera",
+        imageURL: "https://images.squarespace-cdn.com/content/v1/5e7e150d023c3d0568f2042c/8d1c786f-a1a0-4a2a-a14b-e1fd272d42a6/Carlina+Head+shot.jpeg?format=1000w"
+    },
+    bedstuy: {
+        name: "Chi Osse",
+        imageURL: "https://www.politico.com/dims4/default/d2db57a/2147483647/strip/true/crop/2000x2558+0+0/resize/630x806!/quality/90/?url=https%3A%2F%2Fstatic.politico.com%2Fc5%2Fee%2Fbb4cc3794eada341211d95cdf70f%2Fmag.McHugh.Osse.portrait.jpg"
+    }
+}
