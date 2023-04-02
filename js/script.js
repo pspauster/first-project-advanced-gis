6b156c5c6a8d363046c224e05a8f66ace41158e7
Patrick Spauster <patrick.spauster@gmail.com>
2023-04-02 10:50:57 -0400

    code comments

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+//this app adds a photo and name of a councilmemnber to the site when you click on their district name
+
+//create oject with all the name and image data
 const data = {
     lic: {
         name: "Julie Won",
@@ -14,16 +17,18 @@ const data = {
 }
 
 
-
+//function adds html to the DOM
 function setTile(tileHtml) {
     $('#picture-tile').empty().prepend(tileHtml)
 }
 
+//function writes HTML when given the name and image, then calls setTile to add the image to the DOM
 function writeHtml(name, img) {
     var tileHtml = '<img src = ' + '"' + img + '"' + 'id = "image"><h3>' + name + '<h3>';
     setTile(tileHtml)
 }
 
+//grabs the class of the clicked button and passes the matched data to writeHTML
 $('.cd-tile').on('click', function () {
     $('.cd-tile').removeClass("selected")
     const splits = this.className.split(' ');
